22da0e12203a533ac6ac04c0b802ae10ee029fb3
DevWithEasy <devwitheasy@gmail.com>
2023-07-26 14:09:06 +0600

    code added

--- loop.dart
+++ loop.dart
@@ -16,22 +16,32 @@ void main() {
   // print(total);
 
   //Display Even Numbers Between 1 to 5
-  // List even = [];
-  // List odd = [];
+  // List<int> even = [];
+  // List<int> odd = [];
   // int evenTotal = 0;
   // int oddTotal = 0;
 
   // for (var i = 1; i <= 5; i++) {
   //   if (i % 2 == 0) {
   //     even.add(i);
-  //     evenTotal += i;
   //   } else {
   //     odd.add(i);
-  //     oddTotal += i;
   //   }
   // }
   // print({'even': even, 'odd': odd});
+
+  // even.forEach((num) => evenTotal = evenTotal + num);
+  // odd.forEach((num) => oddTotal = oddTotal + num);
+
   // print({'evenTotal': evenTotal, 'oddTotal': oddTotal});
 
-  
+  // List<String> players = ['Ronaldo', 'Messi', 'Neymar', 'Hazard'];
+
+  // for( String player in players){
+  //   print(player);
+  // }
+
+  List<String> footballplayers=['Ronaldo','Messi','Neymar','Hazard'];
+
+  footballplayers.asMap().forEach((index, value) => print("$value index is $index"));
 }
