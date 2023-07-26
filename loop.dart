e6e1eb081cfd311d87860f42eedda123bee07864
DevWithEasy <devwitheasy@gmail.com>
2023-07-26 10:21:28 +0600

    code added

--- loop.dart
+++ loop.dart
@@ -1,3 +1,37 @@
-void main(){
+void main() {
+  //basic loop
+  // var num = 10;
+  // for (var i = 0; i < num; i++) {
+  //   print('Current position is $i');
+  // }
+
+  //Display Sum of Numbers Using For Loop
+  // var total = 0;
+  // var n = 100;
+
+  // for (var i = 0; i <= n; i++) {
+  //   total += i;
+  // }
+
+  // print(total);
+
+  //Display Even Numbers Between 1 to 5
+  // List even = [];
+  // List odd = [];
+  // int evenTotal = 0;
+  // int oddTotal = 0;
+
+  // for (var i = 1; i <= 5; i++) {
+  //   if (i % 2 == 0) {
+  //     even.add(i);
+  //     evenTotal += i;
+  //   } else {
+  //     odd.add(i);
+  //     oddTotal += i;
+  //   }
+  // }
+  // print({'even': even, 'odd': odd});
+  // print({'evenTotal': evenTotal, 'oddTotal': oddTotal});
+
   
 }
